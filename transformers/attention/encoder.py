a8bbf46758d74671d294c6ccf7d52d12da412d6e
Rogers Aloo <owitrogers@gmail.com>
2023-06-07 23:38:38 +0900

    add helper functions

--- transformers/attention/encoder.py
+++ transformers/attention/encoder.py
@@ -8,3 +8,21 @@ import copy
 import time
 import copy
 from torch.optim.lr_schedular import LamdaLR
+import pandas as pd
+import altair as alt
+from torchtext.data.functional import to_map_style_dataset
+from torch.utils.data import DataLoader
+from torchtext.vocab import build_vocab_from_iterator
+import torchtext.datasets as datasets
+import spacy
+import GPUtil
+import warnings
+from torch.utils.data.distributed import DistributedSampler
+import torch.distributed as dist
+import torch.multiprocessing as mp
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+
+# Set to False to skip notebook execution (e.g. for debugging)
+warnings.filterwarnings("ignore")
+RUN_EXAMPLES = True
